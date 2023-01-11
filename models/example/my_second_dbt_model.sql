9acc61fbc29f73e52830fade39cdf637a6c42aa9
raviprasad2212 <raviprasad2545@gmail.com>
2023-01-11 14:09:25 +0530

    Update schema file

--- models/example/my_second_dbt_model.sql
+++ models/example/my_second_dbt_model.sql
@@ -1,5 +1,7 @@
 
--- Use the `ref` function to select from other models
-
-select *
+select
+    email_address,
+    surname,
+    given_name,
+    concat_ws(' ', surname, given_name) as full_name
 from {{ ref('my_first_dbt_model') }}
