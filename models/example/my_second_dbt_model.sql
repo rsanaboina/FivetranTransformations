1eb94ce9468a1125ee91ac75e949d7fa863c909a
rsanaboina <r_sanaboina@hearsaycorp.com>
2023-01-25 20:10:08 +0530

    Remove all files

--- models/example/my_second_dbt_model.sql
+++
@@ -1,7 +0,0 @@
-
-select
-    email_address,
-    surname,
-    given_name,
-    concat_ws(' ', surname, given_name) as full_name
-from {{ ref('my_first_dbt_model') }}
--- models/example/update_states_model.sql
+++
@@ -1,8 +0,0 @@
-select
-    email_address,
-    surname,
-    given_name,
-    concat_ws(' ', surname, given_name) as full_name,
-    'Pune' as city,
-    'Maharashtra' as states
-from {{ ref('my_first_dbt_model') }}
